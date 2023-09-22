2ff62e39a9be1fe17a65b19984cea4fe97e46990
Ahmad Shah Habib <ahmad.shahbcs@gmail.com>
2023-09-22 17:25:08 +0200

    addedd a class

---
+++ src/main/java/EmailSender.java
@@ -0,0 +1,12 @@
+public class EmailSender {
+    private String emailBody;
+    private String emailSubject;
+
+    public String getEmailBody() {
+        return emailBody;
+    }
+
+    public void setEmailBody(String emailBody) {
+        this.emailBody = emailBody;
+    }
+}
